83cf8a64624b720f5754cb8f33e82c4a063c38c0
Stojan Kadiev <kadievstojan@gmail.com>
2021-12-15 13:57:38 +0100

    bookable reviews controller

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\BookableAvailabilityController;
 use App\Http\Controllers\Api\BookableController;
+use App\Http\Controllers\Api\BookableReviewController;
 use App\Models\Bookable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,3 +24,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::apiResource('bookables', BookableController::class);
 Route::get('bookables/{bookable}/availability', BookableAvailabilityController::class)
     ->name('bookables.availability.show');
+Route::get('bookables/{bookable}/reviews', BookableReviewController::class)
+    ->name('bookables.reviews.show');
