b0efc13b2f3c02f41c33321c3f972e2f91dc9808
Stojan Kadiev <kadievstojan@gmail.com>
2021-12-17 11:55:55 +0100

    check if you left review for booking

--- resources/js/components/review/Review.vue
+++ resources/js/components/review/Review.vue
@@ -1,23 +1,32 @@
 <template>
     <div>
-        <div class="form-group">
-            <label class="text-muted">Select the star rating (1 is worst - 5 is best)</label>
-            <star-rating
-                class="fa-3x"
-                v-model="review.rating">
-            </star-rating>
+        <div v-if="loading">Loading</div>
+        <div v-else>
+
+            <div v-if="alreadyReviewed">
+                <h3>You've already left a review for this booking</h3>
+            </div>
+            <div v-else>
+                <div class="form-group">
+                    <label class="text-muted">Select the star rating (1 is worst - 5 is best)</label>
+                    <star-rating
+                        class="fa-3x"
+                        v-model="review.rating">
+                    </star-rating>
+                </div>
+                <div class="form-group">
+                    <label for="content" class="text-muted">Describe your expirience with</label>
+                    <textarea
+                    v-model="review.content"
+                    name="content"
+                    cols="30"
+                    rows="10"
+                    class="form-control"
+                    ></textarea>
+                </div>
+                <button class="btn btn-lg btn-primary w-100 mt-2">Submit</button>
+            </div>
         </div>
-        <div class="form-group">
-            <label for="content" class="text-muted">Describe your expirience with</label>
-            <textarea
-            v-model="review.content"
-            name="content"
-            cols="30"
-            rows="10"
-            class="form-control"
-            ></textarea>
-        </div>
-        <button class="btn btn-lg btn-primary w-100 mt-2">Submit</button>
     </div>
 </template>
 
@@ -28,8 +37,23 @@ export default {
             review: {
                 rating: 5,
                 content: null
-            }
+            },
+            existingReview: null,
+            loading: false
         }
     },
+    created() {
+        this.loading = true;
+        axios.get(`/api/reviews/${this.$route.params.id}`).
+            then((result) => this.existingReview = result.data.data
+            ).catch((err) => {
+                
+            }).then(() => this.loading = false);
+    },
+    computed: {
+        alreadyReviewed() {
+            return this.existingReview !== null;
+        }
+    }
 }
 </script>
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\BookableAvailabilityController;
 use App\Http\Controllers\Api\BookableController;
 use App\Http\Controllers\Api\BookableReviewController;
+use App\Http\Controllers\Api\ReviewController;
 use App\Models\Bookable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -26,3 +27,5 @@ Route::get('bookables/{bookable}/availability', BookableAvailabilityController::
     ->name('bookables.availability.show');
 Route::get('bookables/{bookable}/reviews', BookableReviewController::class)
     ->name('bookables.reviews.show');
+
+Route::apiResource('reviews', ReviewController::class)->only(['show']);
