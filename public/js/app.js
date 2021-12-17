a82e508b5c6dae7881641f11f2d1cab90ebbd3e3
Stojan Kadiev <kadievstojan@gmail.com>
2021-12-17 11:30:23 +0100

    bind star value to review rating

--- public/js/app.js
+++ public/js/app.js
@@ -5630,6 +5630,9 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   data: function data() {
     return {
@@ -5667,18 +5670,18 @@ __webpack_require__.r(__webpack_exports__);
 //
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   props: {
-    rating: Number
+    value: Number
   },
   computed: {
     halfStar: function halfStar() {
-      var fraction = Math.round(this.rating - Math.floor(this.rating)) * 100;
+      var fraction = Math.round(this.value - Math.floor(this.value)) * 100;
       return fraction > 0 && fraction < 50;
     },
     fullStars: function fullStars() {
-      return Math.round(this.rating);
+      return Math.round(this.value);
     },
     emptyStars: function emptyStars() {
-      return 5 - Math.ceil(this.rating);
+      return 5 - Math.ceil(this.value);
     }
   },
   created: function created() {}
@@ -50922,7 +50925,7 @@ var render = function () {
                     [
                       _c("star-rating", {
                         staticClass: "fa-lg",
-                        attrs: { rating: review.rating },
+                        attrs: { value: review.rating },
                       }),
                     ],
                     1
@@ -51095,11 +51098,12 @@ var render = function () {
         _vm._v(" "),
         _c("star-rating", {
           staticClass: "fa-3x",
-          attrs: { rating: _vm.review.rating },
-          on: {
-            "rating:changed": function ($event) {
-              _vm.review.rating = $event
+          model: {
+            value: _vm.review.rating,
+            callback: function ($$v) {
+              _vm.$set(_vm.review, "rating", $$v)
             },
+            expression: "review.rating",
           },
         }),
       ],
@@ -51163,7 +51167,7 @@ var render = function () {
             staticClass: "fas fa-star",
             on: {
               click: function ($event) {
-                return _vm.$emit("rating:changed", star)
+                return _vm.$emit("input", star)
               },
             },
           })
@@ -51179,7 +51183,7 @@ var render = function () {
             staticClass: "far fa-star",
             on: {
               click: function ($event) {
-                return _vm.$emit("rating:changed", _vm.fullStars + star)
+                return _vm.$emit("input", _vm.fullStars + star)
               },
             },
           })
